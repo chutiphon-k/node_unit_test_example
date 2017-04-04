e76f4471a51bfb9afbc851d6543700352d98235a
Chutiphon.k <chutiphon.k@ku.th>
2017-04-04 17:14:00 +0700

    :heavy_plus_sign: Add no-console to .eslintrc

--- src/api/index.js
+++ src/api/index.js
@@ -4,20 +4,20 @@ import { version } from '../../package.json'
 import inventory from '../../data/produce.json'
 
 export default () => {
-	let api = Router();
+	let api = Router()
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
-		res.json({ version });
-	});
+		res.json({ version })
+	})
 
 	api.get('/v1/product', (req, res) => {
-		res.json(inventory);
-	});
+		res.json(inventory)
+	})
 
 	api.get('/v1/product/:id', (req, res) => {
-		const id = parseInt(req.params.id, 10);
-		const record = inventory.find(item => item.id === id);
+		const id = parseInt(req.params.id, 10)
+		const record = inventory.find(item => item.id === id)
 		if (record) {
 			res.json({
 				message: 'Success',
@@ -29,7 +29,6 @@ export default () => {
 				message: `หา item รหัส ${id} ไม่เจอ`
 			})
 		}
-	});
-
+	})
 	return api
 }
