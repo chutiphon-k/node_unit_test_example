94c0bfafbc157f06355a43115fc1188129c9291a
Chutiphon.k <chutiphon.k@ku.th>
2017-04-04 10:26:12 +0700

    :heavy_plus_sign: Update Product test

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
-import { version } from '../../package.json';
-import { Router } from 'express';
+import { Router } from 'express'
+
+import { version } from '../../package.json'
+import inventory from '../../data/produce.json'
 
 export default () => {
 	let api = Router();
@@ -9,5 +11,25 @@ export default () => {
 		res.json({ version });
 	});
 
-	return api;
+	api.get('/v1/product', (req, res) => {
+		res.json(inventory);
+	});
+
+	api.get('/v1/product/:id', (req, res) => {
+		const id = parseInt(req.params.id, 10);
+		const record = inventory.find(item => item.id === id);
+		if (record) {
+			res.json({
+				message: 'Success',
+				item: record
+			})
+		} else {
+			res.status(400).json({
+				status: res.status,
+				message: `หา item รหัส ${id} ไม่เจอ`
+			})
+		}
+	});
+
+	return api
 }
